6101ae0313f38919512837032c51502d477e599f
KubaKocieba <jakub.kocieba@gmail.com>
2020-12-14 12:06:39 +0100

    - removed obsolete stuff

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from "./app.component";
 import { AboutComponent } from "./features/about/about.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "./core/shared.module";
-import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { GifLibraryComponent } from "./features/gif-library/gif-library.component";
 import { AngularFireModule } from "@angular/fire";
 import { environment } from "../environments/environment";
@@ -14,7 +14,6 @@ import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { AngularMaterialModule } from "./core/material/angular-material.module";
 import { LibrarySortPipe } from "./features/gif-library/library-sort.pipe";
 import { LoadingIndicatorComponent } from "./shared/components/loading-indicator/loading-indicator.component";
-import { AngularLoadingBarModule } from "@loadingio/angular-loading-bar";
 import { ImageDetailComponent } from "./features/image-detail/image-detail.component";
 import {
   MAT_SNACK_BAR_DEFAULT_OPTIONS,
@@ -34,7 +33,7 @@ import { LoginErrorComponent } from "./features/login/error/login-error/login-er
 import { EmailNotVerifiedComponent } from "./features/login/email-not-verified/email-not-verified.component";
 import { BigScreenNaviComponent } from "./features/big-screen-navi/big-screen-navi.component";
 import { MobileNaviHeaderComponent } from "./features/mobile-navi/mobile-navi-header/mobile-navi-header.component";
-import { MobileNaviMenuComponent } from './features/mobile-navi/mobile-navi-menu/mobile-navi-menu.component';
+import { MobileNaviMenuComponent } from "./features/mobile-navi/mobile-navi-menu/mobile-navi-menu.component";
 
 @NgModule({
   declarations: [
@@ -67,7 +66,6 @@ import { MobileNaviMenuComponent } from './features/mobile-navi/mobile-navi-menu
     AngularFireAuthModule,
     AngularFireAuthGuardModule,
     AngularFirestoreModule,
-    AngularLoadingBarModule,
     MatSnackBarModule,
     MatSidenavModule,
     ServiceWorkerModule.register("ngsw-worker.js", {
